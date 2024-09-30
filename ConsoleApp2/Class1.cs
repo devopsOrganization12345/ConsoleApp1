7904996e3d2ec1c20b27e6094a30a04389ae6ec4
Aswathy Vikraman <aswathyvikraman95@gmail.com>
2024-09-30 15:25:39 +0530

    Add project files.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp2
+//{
+//    public class program
+//    {
+
+//        public static void Main(string[] args)
+//        {
+//            string input = "hello world";
+//            string[] input1 = input.Split(' ');
+//            foreach (string input2 in input1)
+//            {
+//                char[] output1 = input2.ToCharArray();
+//                Array.Reverse(output1);
+//                string output = new string(output1);
+//                Console.Write(output);
+//            }
+//        }
+//    }
+//}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;      
+using static ConsoleApp2.program;
+
+namespace ConsoleApp2
+{
+   public class ssortingelementsinarray
+    {
+        public static void arraysort()
+        {
+
+            int input = 12345;
+            int sum = 0;
+            while (input > 0)
+            {
+                sum+= input % 10;
+                input /= 10;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+
+    
+    
+    public class program
+    {
+
+       
+        public static void Main(string[] args)
+        {
+            
+            ssortingelementsinarray.arraysort();
+
+        }
+        
+            
+    }
+}
+    
+  
+    
+       
+
+
